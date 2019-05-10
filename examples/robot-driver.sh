cf63f7c500de3e3c1ce42d4400661ded5da9bd0b
Michael Mogenson <michael.mogenson@gmail.com>
2019-05-10 14:16:57 -0400

    Check bluetoothctl version and send appropriatly formated commands

--- examples/robot-driver.sh
+++ examples/robot-driver.sh
@@ -7,7 +7,7 @@
 #               sends pre-defined motor commands based on arrow key presses.
 # Author      : Michael Mogenson
 # Email       : mike@codewithroot.com
-# Copyright   : 2018 Root Robotics All Rights Reserved
+# Copyright   : 2019 Root Robotics All Rights Reserved
 ###############################################################################
 
 ###############################################################################
@@ -40,23 +40,23 @@ declare -r TX_CHAR_UUID="6e400002-b5a3-f393-e0a9-e50e24dcca9e"
 ###############################################################################
 
 # device 1, command 4, left motor speed 100, right motor speed 100
-declare -r forward_cmd="0x01 0x04 0x00 0x00 0x00 0x00 0x64 0x00 0x00 0x00 0x64"`
+declare forward_cmd="0x01 0x04 0x00 0x00 0x00 0x00 0x64 0x00 0x00 0x00 0x64"`
 `" 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0xD1"
 
 # device 1, command 4, left motor speed -100, right motor speed -100
-declare -r back_cmd="0x01 0x04 0x00 0xFF 0xFF 0xFF 0x9C 0xFF 0xFF 0xFF 0x9C"`
+declare back_cmd="0x01 0x04 0x00 0xFF 0xFF 0xFF 0x9C 0xFF 0xFF 0xFF 0x9C"`
 `" 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x71"
 
 # device 1, command 4, left motor speed 0, right motor speed 100
-declare -r left_cmd="0x01 0x04 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x64"`
+declare left_cmd="0x01 0x04 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x64"`
 `" 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x8A"
 
 # device 1, command 4, left motor speed 100, right motor speed 0
-declare -r right_cmd="0x01 0x04 0x00 0x00 0x00 0x00 0x64 0x00 0x00 0x00 0x00"`
+declare right_cmd="0x01 0x04 0x00 0x00 0x00 0x00 0x64 0x00 0x00 0x00 0x00"`
 `" 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x25"
 
 # device 1, command 4, left motor speed 0, right motor speed 0
-declare -r stop_cmd="0x01 0x04 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00"`
+declare stop_cmd="0x01 0x04 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00"`
 `" 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x7E"
 
 ###############################################################################
@@ -65,6 +65,7 @@ declare -r stop_cmd="0x01 0x04 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00"`
 
 declare -a output=""    # empty array to hold output of bluetoothctl
 declare mac_address=""  # empty string to hold MAC address of robot
+declare version="5.43"  # bluetoothctl command syntax changed in v5.48
 
 ###############################################################################
 # Helper functions
@@ -84,13 +85,14 @@ function ble_write {
 }
 
 function disconnect {
-    ble_read false  # drain output of bluetoothctl but don't print
+    (( $(echo "$version >= 5.48" | bc) )) && ble_write "back"
+    ble_read false
     # if the mac address has been set, send the disconnect command
     if [[ -n $mac_address ]]; then
         ble_write "disconnect $mac_address"
-        ble_read true
+        ble_read false
         ble_write "remove $mac_address"
-        ble_read true
+        ble_read false
     fi
     ble_write "exit"  # tell bluetoothctl to exit
 }
@@ -102,20 +104,36 @@ trap disconnect EXIT # call disconnect whenever the script is exited
 
 # start bluetoothctl in background, stdout is COPROC[0], stdin is COPROC[1]
 coproc bluetoothctl
-ble_read true
+ble_read false
+
+# check bluetoothctl version
+ble_write "version"
+ble_read false
+version="${output[1]##* }"
 
 # set filter to only show devices with Root Robot UUID
-ble_write "set-scan-filter-uuids $ROOT_SERVICE_UUID"
-ble_read true
+if (( $(echo "$version >= 5.48" | bc) )); then
+    ble_write "menu scan"
+    ble_write "uuids $ROOT_SERVICE_UUID"
+    ble_write "back"
+    # add quotes around motor commands
+    forward_cmd=\""$forward_cmd"\"
+    back_cmd=\""$back_cmd"\"
+    left_cmd=\""$left_cmd"\"
+    right_cmd=\""$right_cmd"\"
+    stop_cmd=\""$stop_cmd"\"
+else
+    ble_write "set-scan-filter-uuids $ROOT_SERVICE_UUID"
+fi
 
-printf "\nSearching for robot\n\n"
+echo "Searching for robot"
 
 # start scanning for devices
 ble_write "scan on"
 
 # scan until we find a robot and save the MAC address
 while true; do
-    ble_read true                           # read from bluetoothctl
+    ble_read false                          # read from bluetoothctl
     for line in "${output[@]}"; do          # go through output line by line
         if [[ $line =~ $MAC_REGEX ]]; then  # and look for device MAC address
             mac_address=${BASH_REMATCH[1]}  # copy regex match into variable
@@ -126,29 +144,29 @@ done
 
 # stop scanning
 ble_write "scan off"
-ble_read true
 
-printf "\nFound robot, connecting\n\n"
+echo "Found robot, connecting"
 
 # connect to the found MAC address
 ble_write "connect $mac_address"
 
 # search bluetoothctl output for TX characteristic UUID and select it
 while true; do
-    ble_read true                               # read from bluetoothctl
+    ble_read false                              # read from bluetoothctl
     for line in "${output[@]}"; do              # go through output line by line
         if [[ $line = *$TX_CHAR_UUID ]]; then   # and look for TX char UUID
+            (( $(echo "$version >= 5.48" | bc) )) && ble_write "menu gatt"
             # path to TX characteristic is on previous line, remove space first
             ble_write "select-attribute ${previous_line#[[:space:]]}"
-            ble_read true                       # print output
+            ble_read false                      # print output
             break 2                             # break out of both loops
         fi
         previous_line=$line
     done
 done
 
-printf "\nPress arrow keys to drive robot, press any other key to stop\n"
-printf "Use CTRL-C to exit\n\n"
+echo "Press arrow keys to drive robot, press any other key to stop"
+echo "Use CTRL-C to exit"
 
 # read the keyboard arrow keys and send motor commands to robot
 while read -s -r -n 1 key; do   # read one character
@@ -157,25 +175,25 @@ while read -s -r -n 1 key; do   # read one character
             read -s -r -n 2 key # then read two more characters
             case $key in        # see if the two characters are an arrow key
                 "[A")
-                    printf "UP ARROW:\tDRIVE FORWARDS\n"
+                    echo "UP ARROW ----> drive forwards"
                     ble_write "write $forward_cmd"
                     ;;
                 "[B")
-                    printf "DOWN ARROW:\tDRIVE BACKWARDS\n"
+                    echo "DOWN ARROW --> drive backwards"
                     ble_write "write $back_cmd"
                     ;;
                 "[C")
-                    printf "RIGHT ARROW:\tTURN RIGHT\n"
+                    echo "RIGHT ARROW -> turn right"
                     ble_write "write $right_cmd"
                     ;;
                 "[D")
-                    printf "LEFT ARROW:\tTURN LEFT\n"
+                    echo "LEFT ARROW --> turn left"
                     ble_write "write $left_cmd"
                     ;;
             esac
             ;;
-        *)                      # if the character is anything else, stop robot
-            printf "NO ARROW:\tSTOP\n"
+        *)  # if the character is anything else, stop robot
+            echo "NO ARROW ----> stop"
             ble_write "write $stop_cmd"
             ;;
     esac
